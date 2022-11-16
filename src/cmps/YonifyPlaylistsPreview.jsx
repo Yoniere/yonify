297fee8c0c107a85c0de47c1913a408106fae0ec
Yonatan Erez <jontaner12@gmail.com>
2022-11-16 11:34:09 +0200

    playlist grid2

--- src/cmps/YonifyPlaylistsPreview.jsx
+++ src/cmps/YonifyPlaylistsPreview.jsx
@@ -4,13 +4,22 @@ export function YonifyPlaylistsPreview({song}) {
   const hello = ()=> {
     console.log(song)
   }
+   const songDescription=(song)=> {
+    let newDesc=song.snippet.description
+      if (newDesc.length > 50) {
+        newDesc=newDesc.substring(0,51)+'...'
+        
+      } 
+      return newDesc
+    }
+
   return (
     <section className='YonifyPlaylistsPreview grid-item'>
-      <img className='song-img' src={song.snippet.thumbnails.default.url} alt="" />
+      <img className='song-img' src={song.snippet.thumbnails.medium.url} alt="" />
       {hello()}
       <div className='below-img'>
         <h1>{song.snippet.channelTitle}</h1>
-        <p>{song.snippet.description}</p>
+        <p>{songDescription(song)}</p>
       </div>
       
       </section>
