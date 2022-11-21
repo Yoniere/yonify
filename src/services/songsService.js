cf9e620903bbeebbbca45d72e5f6a7fdb842a52c
Yonatan Erez <jontaner12@gmail.com>
2022-11-21 11:17:18 +0200

    songservice fixed

--- src/services/songsService.js
+++ src/services/songsService.js
@@ -7,15 +7,6 @@ export const songsService = {
     query,
 
 }
-// const YT_KEY='AIzaSyD9NhrbLlxuCoYC74C6-rl6Lj5J2eeOir4';
-// async function query(filterBy) {
-//         return await axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&type=video&key=${YT_KEY}&q=${filterBy}`)
-//         .then(res => res.data)
-//         .catch(err => {
-//             console.log('had issus', err)
-//             throw err
-//         })
-//     }
 
 async function query(filterBy) {
         return await axios.get('https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V')
